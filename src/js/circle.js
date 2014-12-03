b4c1ff96999f44c09932b57c81177d469577264a
Yap Hong Kheng <hongkheng.yap@gmail.com>
2014-12-03 17:50:54 +0800

    printing values from fulltilt.js

--- src/js/circle.js
+++ src/js/circle.js
@@ -1,70 +1,88 @@
-(function () {
-	// Check for deviceorientation support
-	if (window.DeviceOrientationEvent) {
-		console.log("DeviceOrientation is supported");
+(function() {
+    // // Check for deviceorientation support
+    // if (window.DeviceOrientationEvent) {
+    //     console.log("DeviceOrientation is supported");
 
-	} else {
-		console.log("DeviceOrientation is not supported!");
-	}
+    // } else {
+    //     console.log("DeviceOrientation is not supported!");
+    // }
 
-	require.config({
-		paths: {
-			es6promise: '../../es6-promise/promise.min',
-			fulltilt: '../../fulltilt/dist/fulltilt'
-		},
-		shim: {
-			'es6promise': {
-				exports: 'es6promise'
-			},
-			'fulltilt': {
-				exports: 'FULLTILT'
-			}
-		}
-	});
+    require.config({
+        paths: {
+            es6promise: '../../es6-promise/promise.min',
+            fulltilt: '../../fulltilt/dist/fulltilt'
+        },
+        shim: {
+            'es6promise': {
+                exports: 'es6promise'
+            },
+            'fulltilt': {
+                exports: 'FULLTILT'
+            }
+        }
+    });
 
-	require(['es6promise','fulltilt'], function(ES6promise, FULLTILT){
-		console.log("ft", ES6promise, FULLTILT);
+    require(['es6promise', 'fulltilt'], function(ES6promise, FULLTILT) {
 
-		var promise = new FULLTILT.getDeviceOrientation({'type':'world'});
+        console.log("ft", ES6promise, FULLTILT);
 
-		var deviceOrientation;
+        var container = document.getElementById("container");
+        var circle = document.getElementById("circle");
+        var containerHeight = window.innerHeight - circle.offsetHeight;
+        var containerWidth = window.innerWidth - circle.offsetWidth;
 
-		promise.then();
+        var promise = new FULLTILT.getDeviceOrientation({
+            'type': 'world'
+        });
+        // FULLTILT.DeviceOrientation instance placeholder
+        var deviceOrientation;
 
-	});
-	// // Circle
-	// var containerHeight = window.innerHeight;
-	// var containerWidth = window.innerWidth;
-	// var container = document.getElementById("container");
+        promise.then(function(controller) {
+            // Store the FULLTILT.DeviceOrientation object
+            deviceOrientation = controller;
+            console.log("done?");
+            draw();
+        }).catch(function(message) {
+            console.error(message);
 
+            // fallback controls
+        });
 
+        function draw() {
 
-	// function handleOrientation(evt){
-	// 	var absolute = evt.absolute;
-	// 	// Device orientation data
-	// 	// z-axis - comes straight up out of the mobile, + towards as it moves up
-	// 	var alpha = evt.alpha;
-	// 	// x-axis - runs side to side across the mobile, + towards the right side [-180, 180]
-	// 	var beta = evt.beta;
-	// 	// y-axis - runs front-to-back across the mobile, + towards as it moves away from you [-90, 90]
-	// 	var gamma = evt.gamma;
+            // var quarternion = deviceOrientation.getScreenAdjustedQuaternion();
+            // var matrix = deviceOrientation.getScreenAdjustedMatrix();
+            // var euler = deviceOrientation.getScreenAdjustedEuler();
+            deviceOrientation.listen(function() {
+                var screenAdjustedEvent = deviceOrientation.getFixedFrameEuler();
+                var euler = deviceOrientation.getScreenAdjustedEuler();
+                // document.getElementById("doTiltLR").innerHTML = Math.round(euler.gamma);
+                // document.getElementById("doTiltFB").innerHTML = Math.round(euler.beta);
+                // document.getElementById("doDirection").innerHTML = Math.round(euler.alpha);
+                document.getElementById("doTiltLR").innerHTML = Math.round(screenAdjustedEvent.gamma);
+                document.getElementById("doTiltFB").innerHTML = Math.round(screenAdjustedEvent.beta);
+                document.getElementById("doDirection").innerHTML = Math.round(screenAdjustedEvent.alpha);
+                var tiltLR = screenAdjustedEvent.gamma;
+                var tiltFB = screenAdjustedEvent.beta;
+                // circle.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+                // circle.style.mozTransform = "rotate(" + tiltLR + "deg)";
+                // circle.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0," + (tiltFB * -1) + "deg)";
 
-	// 	deviceOrientationHandler(gamma, beta, alpha);
+                var x = tiltFB;
+                var y = tiltLR;
 
-	// }
+                circle.style.webkitTransform = "translate3d(" + x + "px," + y + "px, 0)";
+                circle.style.transform = "translate3d(" + x + "px," + y + "px,0)";
 
-	// function deviceOrientationHandler(tiltLR, tiltFB, dir){
-	// 	document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
-	// 	document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
-	// 	document.getElementById("doDirection").innerHTML = Math.round(dir);
+            });
 
-	// 	var circle = document.getElementById("circle");
-	// 	// square.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
-	// 	// square.style.mozTransform = "rotate("+tiltLR +"deg)";
-	// 	// square.style.transform = "rotate("+ tiltLR+"deg) rotate3d(1,0,0,"+ (tiltFB*-1)+"deg)";
+            // console.debug(quarternion);
+            // console.debug(matrix);
+            // console.debug(euler);
 
-	// 	console.log(container.offsetHeight);
-	// }
+        }
+
+    });
 
 
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,83 @@
-(function(){
+(function() {
+    "use strict";
 
-	// Check for deviceorientation support
-	if (window.DeviceOrientationEvent) {
-		console.log("DeviceOrientation is supported");
+    // Check for deviceorientation support
+    if (window.DeviceOrientationEvent) {
+        console.log("DeviceOrientation is supported");
 
-		document.getElementById("doEvent").innerHTML = "DeviceOrientation";
-	
-		// Device orientation
-		window.addEventListener("deviceorientation", handleOrientation, true);
+        document.getElementById("doEvent").innerHTML = "DeviceOrientation";
 
-
-	} else {
-		console.log("DeviceOrientation is not supported!");
-	}
-
-	function handleOrientation(evt){
-		var absolute = evt.absolute;
-		// Device orientation data
-		// z-axis - comes straight up out of the mobile, + towards as it moves up
-		var alpha = evt.alpha;
-		// x-axis - runs side to side across the mobile, + towards the right side [-180, 180]
-		var beta = evt.beta;
-		// y-axis - runs front-to-back across the mobile, + towards as it moves away from you [-90, 90]
-		var gamma = evt.gamma;
-
-		deviceOrientationHandler(gamma, beta, alpha);
-
-	}
-
-	function deviceOrientationHandler(tiltLR, tiltFB, dir){
-		document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
-		document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
-		document.getElementById("doDirection").innerHTML = Math.round(dir);
+        // Device orientation
+        window.addEventListener("deviceorientation", handleOrientation, true);
 
 
-		var square = document.getElementById("square");
-		square.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
-		square.style.mozTransform = "rotate("+tiltLR +"deg)";
-		square.style.transform = "rotate("+ tiltLR+"deg) rotate3d(1,0,0,"+ (tiltFB*-1)+"deg)";
-	}
+    } else {
+        console.log("DeviceOrientation is not supported!");
+    }
 
-	// Check for Device motion events
-	if (window.DeviceMotionEvent) {
-		console.log("DeviceMotionEvent is supported");
+    function handleOrientation(evt) {
+        var absolute = evt.absolute;
+        // Device orientation data
+        // z-axis - comes straight up out of the mobile, + towards as it moves up
+        var alpha = evt.alpha;
+        // x-axis - runs side to side across the mobile, + towards the right side [-180, 180]
+        var beta = evt.beta;
+        // y-axis - runs front-to-back across the mobile, + towards as it moves away from you [-90, 90]
+        var gamma = evt.gamma;
 
-		 window.addEventListener('devicemotion', deviceMotionHandler, false);
-	} else {
-		console.log("DeviceMotionEvent is not supported!");
-		document.getElementById("dmEvent").innerHTML = "Not supported";
-	}
+        deviceOrientationHandler(gamma, beta, alpha);
 
-	function deviceMotionHandler(eventData) {
-	  var info, xyz = "[X, Y, Z]";
+    }
 
-	  // Grab the acceleration from the results
-	  var acceleration = eventData.acceleration;
-	  info = xyz.replace("X", acceleration.x);
-	  info = info.replace("Y", acceleration.y);
-	  info = info.replace("Z", acceleration.z);
-	  document.getElementById("moAccel").innerHTML = info;
+    function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+        document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+        document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+        document.getElementById("doDirection").innerHTML = Math.round(dir);
 
-	  // Grab the acceleration including gravity from the results
-	  acceleration = eventData.accelerationIncludingGravity;
-	  info = xyz.replace("X", acceleration.x);
-	  info = info.replace("Y", acceleration.y);
-	  info = info.replace("Z", acceleration.z);
-	  document.getElementById("moAccelGrav").innerHTML = info;
 
-	  // Grab the rotation rate from the results
-	  var rotation = eventData.rotationRate;
-	  info = xyz.replace("X", rotation.alpha);
-	  info = info.replace("Y", rotation.beta);
-	  info = info.replace("Z", rotation.gamma);
-	  document.getElementById("moRotation").innerHTML = info;
+        var square = document.getElementById("square");
+        square.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+        square.style.mozTransform = "rotate(" + tiltLR + "deg)";
+        square.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0," + (tiltFB * -1) + "deg)";
+    }
 
-	  // // Grab the refresh interval from the results
-	  info = eventData.interval;
-	  document.getElementById("moInterval").innerHTML = info;       
-	}
-	
+    // Check for Device motion events
+    if (window.DeviceMotionEvent) {
+        console.log("DeviceMotionEvent is supported");
+
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
+    } else {
+        console.log("DeviceMotionEvent is not supported!");
+        document.getElementById("dmEvent").innerHTML = "Not supported";
+    }
+
+    function deviceMotionHandler(eventData) {
+        var info, xyz = "[X, Y, Z]";
+
+        // Grab the acceleration from the results
+        var acceleration = eventData.acceleration;
+        info = xyz.replace("X", acceleration.x);
+        info = info.replace("Y", acceleration.y);
+        info = info.replace("Z", acceleration.z);
+        document.getElementById("moAccel").innerHTML = info;
+
+        // Grab the acceleration including gravity from the results
+        acceleration = eventData.accelerationIncludingGravity;
+        info = xyz.replace("X", acceleration.x);
+        info = info.replace("Y", acceleration.y);
+        info = info.replace("Z", acceleration.z);
+        document.getElementById("moAccelGrav").innerHTML = info;
+
+        // Grab the rotation rate from the results
+        var rotation = eventData.rotationRate;
+        info = xyz.replace("X", rotation.alpha);
+        info = info.replace("Y", rotation.beta);
+        info = info.replace("Z", rotation.gamma);
+        document.getElementById("moRotation").innerHTML = info;
+
+        // // Grab the refresh interval from the results
+        info = eventData.interval;
+        document.getElementById("moInterval").innerHTML = info;
+    }
+
 })();
